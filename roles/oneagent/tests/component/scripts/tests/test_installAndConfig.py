02606422e8243cf173f6dad46b48d202e43adf70
Jakub Rak <jakub.rak@dynatrace.com>
2024-10-10 16:39:20 +0200

    Refactor test_installAndConfig.py

--- roles/oneagent/tests/component/scripts/tests/test_installAndConfig.py
+++ roles/oneagent/tests/component/scripts/tests/test_installAndConfig.py
@@ -20,25 +20,11 @@ TECH_NAME_KEY = "orchestration_tech"
 TECH_VERSION_KEY = "tech_version"
 TECH_SCRIPT_VERSION_KEY = "script_version"
 
-# Tags and properties for regular installation
-INSTALLER_TAG = "install_tag"
-INSTALLER_PROPERTY = "install_prop1=install_1"
-PLATFORM_TAG = "platform_install_tag"
-PLATFORM_PROPERTY = "platform_install_property"
+CTL_OPTION_GET_HOST_TAGS = "--get-host-tags"
+CTL_OPTION_SET_HOST_TAG = "--set-host-tag"
 
-# Tags and properties for intended configuration with oneagentctl
-CONFIG_INSTALL_TAG = "config_install_tag"
-CONFIG_INSTALL_PROPERTY = "install_prop2=config_install_prop"
-CONFIG_INTENDED_TAG = "config_intended_tag"
-CONFIG_INTENDED_PROPERTY = "install_prop2=config_intended_prop"
-
-CTL_TAGS_GETTER = "--get-host-tags"
-CTL_PROPERTIES_GETTER = "--get-host-properties"
-
-INSTALLER_ARGS = [f'--set-host-tag="{INSTALLER_TAG}"', f'--set-host-property="{INSTALLER_PROPERTY}"']
-INSTALLER_PLATFORM_ARGS = [f'--set-host-tag="{PLATFORM_TAG}"', f'--set-host-property="{PLATFORM_PROPERTY}"']
-CONFIG_INSTALL_ARGS = [f'--set-host-tag="{CONFIG_INSTALL_TAG}"', f'--set-host-property="{CONFIG_INSTALL_PROPERTY}"']
-CONFIG_INTENDED_ARGS = [f'--set-host-tag="{CONFIG_INTENDED_TAG}"', f'--set-host-property="{CONFIG_INTENDED_PROPERTY}"']
+CTL_OPTION_GET_HOST_PROPERTIES = "--get-host-properties"
+CTL_OPTION_SET_HOST_PROPERTY = "--set-host-property"
 
 
 def _assert_oneagentctl_getter(
@@ -50,18 +36,13 @@ def _assert_oneagentctl_getter(
 ):
     oneagentctl = f"{get_oneagentctl_path(platform)}"
     result = wrapper.run_command(platform, address, oneagentctl, ctl_param)
-    assert result.returncode == 0 and expected_values == set(result.stdout.strip().splitlines())
+    assert result.returncode == 0
+    assert expected_values == set(result.stdout.strip().splitlines())
 
 
 def _check_install_args(
-    platform: DeploymentPlatform, address: str, wrapper: PlatformCommandWrapper, technology: str
-) -> None:
+    platform: DeploymentPlatform, address: str, wrapper: PlatformCommandWrapper, technology: str) -> None:
     logging.debug("Platform: %s, IP: %s", platform, address)
-
-    _assert_oneagentctl_getter(platform, address, wrapper, CTL_TAGS_GETTER, {INSTALLER_TAG, PLATFORM_TAG})
-    _assert_oneagentctl_getter(
-        platform, address, wrapper, CTL_PROPERTIES_GETTER, {INSTALLER_PROPERTY, PLATFORM_PROPERTY}
-    )
 
     oneagentctl = f"{get_oneagentctl_path(platform)}"
     metadata = wrapper.run_command(platform, address, oneagentctl, "--get-deployment-metadata")
@@ -77,8 +58,8 @@ def _check_config_args(platform: DeploymentPlatform, address: str, wrapper: Plat
                        expected_properties: set[str]):
     logging.debug("Platform: %s, IP: %s", platform, address)
 
-    _assert_oneagentctl_getter(platform, address, wrapper, CTL_TAGS_GETTER, expected_tags)
-    _assert_oneagentctl_getter(platform, address, wrapper, CTL_PROPERTIES_GETTER, expected_properties)
+    _assert_oneagentctl_getter(platform, address, wrapper, CTL_OPTION_GET_HOST_TAGS, expected_tags)
+    _assert_oneagentctl_getter(platform, address, wrapper, CTL_OPTION_GET_HOST_PROPERTIES, expected_properties)
 
 
 def _check_output_for_secrets(result: DeploymentResult) -> None:
@@ -90,15 +71,23 @@ def _check_output_for_secrets(result: DeploymentResult) -> None:
 def test_basic_installation(_set_up, _tear_down, runner, configurator, constants, platforms, wrapper):
     logging.info("Running basic installation test")
 
+    dummy_common_tag = "dummy_common_tag"
+    dummy_platform_tag = "dummy_platform_tag"
+    dummy_common_property = "dummy_common_key=dummy_common_value"
+    dummy_platform_property = "dummy_platform_key=dummy_platform_value"
+
     set_installer_download_params(configurator)
     configurator.set_common_parameter(configurator.PRESERVE_INSTALLER_KEY, True)
-    configurator.set_common_parameter(configurator.INSTALLER_ARGS_KEY, INSTALLER_ARGS)
-    configurator.set_common_parameter(configurator.VERIFY_SIGNATURE_KEY, False)
+    configurator.set_common_parameter(configurator.INSTALLER_ARGS_KEY,
+                                      [f"{CTL_OPTION_SET_HOST_TAG}={dummy_common_tag}",
+                                       f"{CTL_OPTION_SET_HOST_PROPERTY}={dummy_common_property}"])
 
     for platform, _ in platforms.items():
         download_dir: Path = get_platform_argument(platform, UNIX_DOWNLOAD_PATH, WINDOWS_DOWNLOAD_PATH)
         configurator.set_platform_parameter(platform, configurator.DOWNLOAD_DIR_KEY, str(download_dir))
-        configurator.set_platform_parameter(platform, configurator.INSTALLER_PLATFORM_ARGS_KEY, INSTALLER_PLATFORM_ARGS)
+        configurator.set_common_parameter(configurator.INSTALLER_PLATFORM_ARGS_KEY,
+                                          [f"{CTL_OPTION_SET_HOST_TAG}={dummy_platform_tag}",
+                                           f"{CTL_OPTION_SET_HOST_PROPERTY}={dummy_platform_property}"])
 
     result = run_deployment(runner)
 
@@ -113,48 +102,16 @@ def test_basic_installation(_set_up, _tear_down, runner, configurator, constants
         platforms, check_download_directory, wrapper, True, UNIX_DOWNLOAD_PATH, WINDOWS_DOWNLOAD_PATH
     )
 
+
+    logging.info("Check if config args were applied correctly")
+    perform_operation_on_platforms(platforms,
+                                   _check_config_args,
+                                   wrapper,
+                                   {dummy_common_tag, dummy_platform_tag},
+                                   {dummy_common_property, dummy_platform_property})
+
     logging.info("Check if installer args were passed correctly")
     perform_operation_on_platforms(platforms, _check_install_args, wrapper, constants.TECH_NAME)
 
 
-def test_oneagentctl_installation_config(_set_up, runner, configurator, platforms, wrapper):
-    logging.info("Running oneagentctl config test")
 
-    configurator.clear_parameters_section()
-    set_installer_download_params(configurator)
-    configurator.set_common_parameter(configurator.INSTALLER_ARGS_KEY, CONFIG_INSTALL_ARGS)
-    configurator.set_common_parameter(configurator.VERIFY_SIGNATURE_KEY, False)
-
-    run_deployment(runner)
-
-    logging.info("Check if config args were applied correctly")
-    expected_tags = {INSTALLER_TAG, PLATFORM_TAG, CONFIG_INSTALL_TAG}
-    expected_properties = {PLATFORM_PROPERTY, CONFIG_INSTALL_PROPERTY, INSTALLER_PROPERTY}
-    perform_operation_on_platforms(platforms, _check_config_args, wrapper, expected_tags, expected_properties)
-
-
-def test_oneagentctl_intended_config(_set_up, runner, configurator, platforms, wrapper):
-    logging.info("Running oneagentctl config test")
-
-    configurator.clear_parameters_section()
-    configurator.set_common_parameter(configurator.INSTALLER_ARGS_KEY, CONFIG_INTENDED_ARGS)
-    configurator.set_common_parameter(configurator.VERIFY_SIGNATURE_KEY, False)
-
-    run_deployment(runner)
-
-    logging.info("Check if config args were applied correctly")
-    expected_tags = {INSTALLER_TAG, PLATFORM_TAG, CONFIG_INSTALL_TAG, CONFIG_INTENDED_TAG}
-    expected_properties = {PLATFORM_PROPERTY, CONFIG_INTENDED_PROPERTY, INSTALLER_PROPERTY}
-
-    perform_operation_on_platforms(platforms, _check_config_args, wrapper, expected_tags, expected_properties)
-
-
-def test_uninstall(_set_up, runner, configurator, platforms, wrapper):
-    logging.info("Running uninstall test")
-
-    configurator.set_common_parameter(configurator.PACKAGE_STATE_KEY, "absent")
-
-    run_deployment(runner)
-
-    logging.info("Check if agent is uninstalled")
-    perform_operation_on_platforms(platforms, check_agent_state, wrapper, False)
