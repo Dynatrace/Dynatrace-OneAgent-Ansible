51418f63af31d3d13c1bb8e76f4e27118c0bfefe
Jakub Rak <jakub.rak@dynatrace.com>
2024-10-15 14:43:25 +0200

    Remove unused ssh connection handling

--- roles/oneagent/tests/component/scripts/command/unix/unix_command_wrapper.py
+++ roles/oneagent/tests/component/scripts/command/unix/unix_command_wrapper.py
@@ -1,26 +1,32 @@
 from pathlib import Path
 
 from command.command_wrapper import CommandWrapper
-from command.unix.unix_command_executor import UnixCommandExecutor
 from util.test_data_types import CommandResult
+
+import subprocess
 
 
 class UnixCommandWrapper(CommandWrapper):
     def __init__(self, user: str, password: str):
         self.password = password
-        self.executor = UnixCommandExecutor(user, password)
+
+    def _execute(self, address: str, command: str, *args: str) -> CommandResult:
+        out = subprocess.run(
+            " ".join([command, *args]), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, check=False, shell=True
+        )
+        return CommandResult(out.returncode, out.stdout, out.stderr)
 
     def get_file_content(self, address: str, file: Path) -> CommandResult:
-        return self.executor.execute(address, "cat", str(file))
+        return self._execute(address, "cat", str(file))
 
     def file_exists(self, address: str, file: Path) -> CommandResult:
-        return self.executor.execute(address, "test", "-f", str(file))
+        return self._execute(address, "test", "-f", str(file))
 
     def directory_exists(self, address: str, directory: Path) -> CommandResult:
-        return self.executor.execute(address, "test", "-d", str(directory))
+        return self._execute(address, "test", "-d", str(directory))
 
     def create_directory(self, address: str, directory: Path) -> CommandResult:
-        return self.executor.execute(address, "mkdir", "-p", str(directory))
+        return self._execute(address, "mkdir", "-p", str(directory))
 
     def run_command(self, address: str, command: str, *args: str) -> CommandResult:
-        return self.executor.execute(address, f"echo {self.password} | sudo -S {command}", *args)
+        return self._execute(address, f"echo {self.password} | sudo -S {command}", *args)
